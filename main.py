ae99dc7a7d3978a411ff0e1d5e46eca347ff4813
John Mann <john@foxdata.io>
2023-05-09 18:41:19 -0500

    Add code to auto expand tables when exporting.

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import csv
 import sqlite3
 
-db_file = ''
-db_table = ''
+# Connect to database
+db_file = 'main.sqlite'
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
-data = cur.execute(f"SELECT * FROM {db_table}")
 
-with open('output.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerows(data)
+# Get all table names
+db_list = []
+for db_name in cur.execute("SELECT name FROM sqlite_master WHERE type = 'table'"):
+    db_list.append(db_name)
+
+# Export all tables to csv
+for table in db_list:
+    try:
+        cur.execute(f"SELECT * FROM {table[0]}")
+        rows = cur.fetchall()
+        with open(f"{table[0]}.csv", 'w', newline='') as table_csv:
+            writer = csv.writer(table_csv)
+            writer.writerows(rows)
+    except sqlite3.Error as e:
+        continue
+
+# Close connection
+conn.close()
